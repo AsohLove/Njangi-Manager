bd44fd7ab8e8df0d88c4b178d55f5651a879549e
KBrand <kbrandn25@gmail.com>
2026-09-23 11:14:00 +0100

    feat: enable to work on the network

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.21"],
+
+  // Add your rewrites configuration here
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*", // Maps to your backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
