d5b3194441adf78576b63873e03866d866729ede
KBrand <kbrandn25@gmail.com>
2026-09-25 08:44:33 +0100

    feat: remove unused configuration from next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.100.18"],
-
-  // Add your rewrites configuration here
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:4000/api/:path*", // Maps to your backend
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
